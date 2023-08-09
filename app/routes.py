623d5a6ff252098b693575f0e2110a027bf179b5
Emilio Marcos <emilio7marcos@gmail.com>
2023-08-09 04:15:40 -0600

    add pypdf2

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from flask import request
 from app import app
 from bots import bot1, bot2
 from werkzeug.utils import secure_filename
+import pypdf
 
 ALLOWED_EXTENSIONS = {'pdf'}
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
@@ -12,6 +13,13 @@ def allowed_file(filename):
     filename = secure_filename(filename)
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def is_pdf(file_stream):
+    try:
+        pypdf.PdfFileReader(file_stream)
+        return True
+    except pypdf.PdfReadError:
+        return False
 
 @app.route('/')
 def root():
@@ -33,7 +41,14 @@ def run_bot2():
 
     if file and allowed_file(file.filename):
         with tempfile.NamedTemporaryFile() as temp:
-            temp.write(file.read())
+            file.save(temp)
+            temp.seek(0)
+
+            try:
+                pypdf.PdfFileReader(temp.name)
+            except:
+                return 'Invalid PDF', 400
+
             file_path = temp.name
 
             os.chmod(file_path, 0o600)
