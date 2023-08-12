462bbb0df2f3e8cb61ba4bfa36f9fea003c59e20
Emilio Marcos <emilio7marcos@gmail.com>
2023-08-12 08:25:04 -0600

    explicit route CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173"])
+# CORS(app, origins=["http://localhost:5173"])
 
 from app import routes
