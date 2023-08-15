8841435138585476dc4fb2545d18550b1587ff98
Emilio Marcos <emilio7marcos@gmail.com>
2023-08-14 22:03:56 -0600

    add deployed origin

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173"])
+CORS(app, origins=["http://localhost:5173", "https://bots2learn.com"])
 
 from app import routes
