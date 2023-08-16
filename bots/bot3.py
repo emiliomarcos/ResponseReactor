9bcd8f042c444cd76703866e2c89f9116681d98b
Emilio Marcos <emilio7marcos@gmail.com>
2023-08-16 02:41:02 -0600

    bot3

--- bots/bot3.py
+++ bots/bot3.py
@@ -9,7 +9,8 @@ def run():
 
     agent = create_csv_agent(OpenAI(temperature=0, openai_api_key=openai_api_key), "./data/doug_score.csv", verbose=True)
 
-    car_question = "what are the 10 highest rated cars as a daily?"
+    car_question = "what are the 5 highest rated and 5 lowest rated cars from their dougscore? Give me the name and dougscore"
+    # car_question = "what are the three most common vehicle countries and with how many vehicles?"
 
     bot_response = agent.run(car_question)
 
