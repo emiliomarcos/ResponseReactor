d4df22e8bf6660195755dcb8c663f2674fd551f3
Emilio Marcos <emilio7marcos@gmail.com>
2023-08-16 02:01:54 -0600

    csv bot functionality

--- bots/bot3.py
+++ bots/bot3.py
@@ -6,3 +6,11 @@ from langchain.llms import OpenAI
 def run():
     load_dotenv()
     openai_api_key = os.getenv('OPENAI_API_KEY')
+
+    agent = create_csv_agent(OpenAI(temperature=0, openai_api_key=openai_api_key, model_name="gpt-3.5-turbo-16k"), "./data/doug_score.csv", verbose=True)
+
+    car_question = "what are the 10 highest rated cars as a daily?"
+
+    bot_response = agent.run(car_question)
+
+    print(bot_response)
